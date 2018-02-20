1c426dadcda6cb8d5adf49e3704272aa0177de65
Nicholas Grout <njgrout@gmail.com>
2018-02-20 11:42:39 -0800

    modified:   ../.gitignore 	modified:   ../CMakeLists.txt 	renamed:    ../get_data.py -> get_data.py 	modified:   ../main.cpp 	deleted:    ../src/count_words.cpp 	deleted:    ../src/count_words.h 	deleted:    ../src/make_window.cpp 	deleted:    ../src/make_window.h 	deleted:    ../src/parse.cpp 	deleted:    ../src/parse.h 	deleted:    ../src/support.cpp 	deleted:    ../src/support.h 	deleted:    ../src/window.cpp 	deleted:    ../src/window.h

--- main.cpp
+++ main.cpp
@@ -1,51 +1,12 @@
-#include "src/support.h"
-#include "src/count_words.h"
-#include "src/window.h"
-#include "src/make_window.h"
+#include "parse.h"
+#include <iostream>
 
 using std::cout;
 using std::endl;
 
-/* Testing functions */
-void test_window(const std::vector<string> &files);
-void test_count(const std::vector<string> &files);
-void test_make_windows(const std::vector<string> &files);
-
-
 int main(int argc, char *argv[])
 {
-    auto f = glob_files(argv[1]);
-
-    // Clean text, lower capital letters
-    for(auto &a : f )
-        clean(a);
-
-    //test_window(f);
-    //test_count(f);
-    test_make_windows(f);
+    cout << "THIS" << endl;
     return 0;
 }
 
-
-void test_window(const std::vector<string> &files)
-{
-}
-
-
-void test_count(const std::vector<string> &files)
-{
-    count_words count(files);
-    auto sorted = count.sort();
-    for (auto const &a : sorted )
-        cout << a.first << ',' << a.second << endl;
-}
-
-void test_make_windows(const std::vector<string> &files)
-{
-    cout << "First file: " << files[0] << endl;
-    make_window maker(files);
-    /*
-    for(auto const & a: maker)
-        cout << "window: " << a << endl;
-    */
-}
