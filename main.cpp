4675f040af64388bb10d2cdd4c661026acb9e500
Nicholas Grout <njgrout@gmail.com>
2018-02-20 12:01:15 -0800

    modified:   .gitignore 	modified:   main.cpp 	modified:   src/vectorize/CMakeLists.txt 	modified:   src/vectorize/parse.h

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include "parse.h"
 #include <iostream>
+#include <fstream>
 
 using std::cout;
 using std::endl;
 
 int main(int argc, char *argv[])
 {
-    cout << "THIS" << endl;
+    std::ifstream fin(argv[1]);
+    char buff[1024];
+
+    fin.get(buff, 1024, '\n');
+    for(uint i = 0; i < fin.gcount(); ++i)
+    {
+        cout << buff[i];
+    }
+
+    cout << "done" << endl;
+
     return 0;
 }
 
