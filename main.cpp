4b73a01d4c9b60d59831681f5685a3a185fc05ec
Nicholas Grout <njgrout@gmail.com>
2018-02-23 12:31:26 -0800

    modified:   data/test/UTF8/UTF8.txt 	new file:   data/test/UTF8/sample.txt 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -7,37 +7,93 @@
 using std::cout;
 using std::endl;
 
-void show_hex(char *s, int len)
+/*
+ * So for UTF-8 encoding:
+ *  0x00 - 0x7f is regular 1 byte ASCII character
+ *  0x80 - 0xBF means this is a continuing byte for a multi-byte character
+ *  0xC2 - 0xDF is the beginning byte of a two byte character
+ *  0xE0 - 0xEF is the beginning byte of a three byte character
+ *  0xF0 - 0xFF is the beginning byte of a four byte sequence
+ *
+ *  len      1st byte
+ *  1 bytes  0xxxxxxx
+ *  2 bytes  110xxxxx
+ *  3 bytes  1110xxxx
+ *  4 bytes  11110xxx
+ */
+
+int parse_utf8(char *str, int len)
 {
-    char c;
-    for(uint i = 0; i < len; ++i)
+    int num_chars = 0;
+    uint8_t byte;
+
+    for (int i = 0; i < len && *str; ++i)
     {
-        c = s[i];
-        printf("%x ", c);
-        if( c == 0x27 )
+        // Parse individual characters
+        byte = (uint8_t) *str;
+
+        // One byte character
+        if (byte <= 0x7f)
         {
-            printf("That's it!\n");
+            printf("%c",*str);
+            ++str;
         }
+
+        // Two byte character
+        else if (byte >= 0xc2 && byte <= 0xdf)
+        {
+            for(int i = 0; i < 2; ++i)
+            {
+                printf("%c",*str);
+                ++str;
+            }
+        }
+
+        // Three byte character
+        else if (byte >= 0xe0 && byte <= 0xef)
+        {
+            for(int i = 0; i < 3; ++i)
+            {
+                printf("%c",*str);
+                ++str;
+            }
+        }
+
+        // Four byte character
+        else if (byte >= 0xf0)
+        {
+            for(int i = 0; i < 4; ++i)
+            {
+                printf("%c",*str);
+                ++str;
+            }
+        }
+
+        num_chars += 1;
+
     }
     printf("\n");
+
+    return num_chars;
 }
 
 int main(int argc, char *argv[])
 {
     std::ifstream fin(argv[1]);
-    char buff[1024];
+    int len;
+    char *buff;
 
-    fin.get(buff, 1024, '\n');
-    fin.ignore(1024, '\n');
+    fin.seekg (0, fin.end);
+    len = fin.tellg();
+    fin.seekg (0, fin.beg);
 
-    while( !fin.eof() )
-    {
-        cout << buff << endl;
-        show_hex(buff, strlen(buff));
+    cout << len << endl;
+    buff = new char[len + 1];
 
-        fin.get(buff, 1024, '\n');
-        fin.ignore(1024, '\n');
-    }
+    fin.get(buff, len, '\0');
+    fin.ignore(len, '\0');
+
+    cout << "Number of chars: " << parse_utf8(buff, strlen(buff) ) << endl;
 
     return 0;
 }
