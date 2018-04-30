8323562e21e0bed12a3ea1df17e378ffad8fccc8
nick grout <njgrout@gmail.com>
2018-04-30 04:15:19 -0700

    modified:   tests/b_tree/CMakeLists.txt 	modified:   tests/b_tree/main.cpp

--- tests/b_tree/main.cpp
+++ tests/b_tree/main.cpp
@@ -6,24 +6,84 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <zconf.h>
 using namespace std;
 
-void test_atom(atomic_int &c)
+void test_mutex(int &c, mutex &int_lock)
 {
-    auto temp = c.load();
-    temp++;
-    c = temp;
+    lock_guard<mutex> guard(int_lock);
+    for (int i=0; i < 10; ++i)
+    {
+        ++c;
+        usleep(rand() % 10);
+    }
+}
+
+void test_atomic(atomic_int &c)
+{
+    for (int i=0; i < 10; ++i)
+    {
+        ++c;
+        usleep(rand() % 10);
+    }
+
+}
+
+void test_reg(int &c)
+{
+    for (int i=0; i < 10; ++i)
+    {
+        ++c;
+        usleep(rand() % 10);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    atomic_int c;
-    c.store(0);
-    for (int i = 0; i < 10; ++i)
+    auto T_SIZE = 10;
+    thread threads[T_SIZE];
+
+    // Mutex testing
     {
-        thread t(test_atom, std::ref(c));
-        t.detach();
+        int mutex_int = 0;
+        mutex int_lock;
+        for (int i = 0; i < T_SIZE; ++i) {
+            threads[i] = thread(test_mutex, std::ref(mutex_int), std::ref(int_lock));
+        }
+        for (int i = 0; i < T_SIZE; ++i)
+            threads[i].join();
+        cout << "Mutex int: " << mutex_int << endl;
+        cout << "Mutex size: " << sizeof(mutex_int) << endl;
+        cout << endl;
     }
-    cout << c << endl;
+
+    {
+        atomic_int atom_int;
+        atom_int = 0;
+        // Atomic testing
+        for (int i = 0; i < T_SIZE; ++i)
+        {
+            threads[i] = thread(test_atomic, std::ref(atom_int));
+        }
+        for (int i = 0; i < T_SIZE; ++i)
+            threads[i].join();
+        cout << "Atomic int: " << atom_int << endl;
+        cout << "Atomic size: " << sizeof(atom_int) << endl;
+        cout << endl;
+    }
+
+    {
+        // Regular int testing
+        int reg_int = 0;
+        for (int i = 0; i < T_SIZE; ++i)
+        {
+            threads[i] = thread(test_reg, std::ref(reg_int));
+        }
+        for (int i = 0; i < T_SIZE; ++i)
+            threads[i].join();
+        cout << "regular: " << reg_int << endl;
+        cout << "regular size: " << sizeof(reg_int) << endl;
+        cout << endl;
+    }
 }
 
