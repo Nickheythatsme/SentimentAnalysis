7090dc96e6c02563dae5f6367136a1543962df74
nick grout <njgrout@gmail.com>
2018-05-27 16:05:16 -0700

    modified:   ../../.gitignore 	deleted:    ../../.idea/SentimentAnalysis.iml 	deleted:    ../../.idea/codeStyles/Project.xml 	deleted:    ../../.idea/codeStyles/codeStyleConfig.xml 	deleted:    ../../.idea/misc.xml 	deleted:    ../../.idea/modules.xml 	deleted:    ../../.idea/vcs.xml 	modified:   .idea/workspace.xml 	modified:   worker.hpp

--- prototypes/queue/worker.hpp
+++ prototypes/queue/worker.hpp
@@ -1,10 +1,23 @@
+/*
+ * 5/27/2018 Nick Grout
+ *
+ * This class manages one thread, completing a job at a time until indicated to stop, or until the queue is empty.
+ * We use condition_variables to determine whether the queue has data in it. The condition is called by the
+ * manager class, thread_queue (in separate file). This way we don't eat up processor time and we also don't
+ * need to wait until the next cycle to check if data exists.
+ *
+ * We use atomic variables and mutexes to ensure that there are no race conditions surrounding our queue.
+ */
 #ifndef QUEUE_WORKER_
 #include QUEUE_WORKER_
 
-
 #include <condition_variable>
 #include <atomic>
+#include <mutex>
+#include <queue>
+#include "job.hpp"
 
+// TODO finish implementation
 template<typename R, typename A>
 class worker
 {
@@ -15,7 +28,7 @@ class worker
         ~worker();
     protected:
     private:
-        std::queue<R
+        std::queue<job<R,A>> jobs;
 };
 
 template<typename R, typename A>
