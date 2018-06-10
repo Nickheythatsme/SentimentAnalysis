d8992964e25712680b3ee03a84d279f1548746c6
nick grout <njgrout@gmail.com>
2018-06-09 17:26:05 -0700

    modified:   ../tests/queue/main.cpp

--- tests/queue/main.cpp
+++ tests/queue/main.cpp
@@ -1,4 +1,5 @@
 #include "../testing_classes/case.hpp"
+#include "../testing_classes/performance.hpp"
 #include "tqueue.hpp"
 #include "matrix.h"
 #include <iostream>
@@ -23,6 +24,7 @@ bool run_test()
 
     for (const auto &a : args)
     {
+        cout << "Starting thread queue with " << a << " threads" << endl;
         thread_queue<int, matrix&> tq (test_func, std::vector<matrix>(jobs), a);
         tq.start();
     }
@@ -30,11 +32,30 @@ bool run_test()
     return true;
 }
 
+bool performance_func(size_t n)
+{
+    matrix m;
+    m.fill_random(1000, 1000);
+    std::vector<matrix> jobs;
+    for (int i=0; i < 100; ++i)
+        jobs.push_back(matrix(m));
+
+    cout << "Starting thread queue with " << n << " threads" << endl;
+    thread_queue<int, matrix&> tq (test_func, std::vector<matrix>(jobs), n);
+    tq.start();
+
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     test_case case1 ("multi threaded queue test on various threads", run_test);
-    case1.start();
-    cout << case1.get_result() << endl;
+    // case1.start();
+    // cout << case1.get_result() << endl;
+
+    performance_test perf1("performance test of queue", performance_func, 10, 80); 
+    perf1.start();
+    cout << perf1.get_result() << endl;
 
 
     return !(case1.get_result().passed);
