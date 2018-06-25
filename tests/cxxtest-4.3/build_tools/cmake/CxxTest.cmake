47dc304fcafa30e2c2590c82a53454f960874b8a
Nicholas Grout <njgrout@gmail.com>
2018-06-25 14:30:24 -0700

    modified:   ../tests/CMakeLists.txt 	modified:   ../tests/cxxtest-4.3/build_tools/cmake/CxxTest.cmake 	modified:   ../tests/parse/CMakeLists.txt

--- tests/cxxtest-4.3/build_tools/cmake/CxxTest.cmake
+++ tests/cxxtest-4.3/build_tools/cmake/CxxTest.cmake
@@ -3,11 +3,10 @@ include("${CMAKE_CURRENT_LIST_DIR}/FindCxxTest.cmake")
 
 function(cxx_test target source)
     string(REGEX REPLACE "hpp$" "cpp" CPP_FILE_NAME ${source})
-    message(${CPP_FILE_NAME})
     set(CPP_FULL_NAME "${CMAKE_CURRENT_BINARY_DIR}/${CPP_FILE_NAME}")
     add_custom_command(
         OUTPUT "${CPP_FULL_NAME}"
-        COMMAND ${CXXTESTGEN} --runner=ErrorPrinter --output "${CPP_FULL_NAME}" "${source}"
+        COMMAND ${CXXTESTGEN} --runner=ErrorPrinter --output "${CPP_FULL_NAME}" "${source}" 
         DEPENDS "${source}"
     )
     add_executable(${target} ${CPP_FULL_NAME})
