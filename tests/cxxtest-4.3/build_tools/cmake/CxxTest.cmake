915a60f0a64c3a95817bb1644ab7333c42973cb0
Nicholas Grout <njgrout@gmail.com>
2018-06-25 17:17:03 -0700

    modified:   ../CMakeLists.txt 	modified:   ../tests/CMakeLists.txt 	modified:   ../tests/cxxtest-4.3/build_tools/cmake/CxxTest.cmake 	modified:   ../tests/parse/CMakeLists.txt

--- tests/cxxtest-4.3/build_tools/cmake/CxxTest.cmake
+++ tests/cxxtest-4.3/build_tools/cmake/CxxTest.cmake
@@ -2,17 +2,14 @@
 include("${CMAKE_CURRENT_LIST_DIR}/FindCxxTest.cmake")
 
 function(cxx_test target source)
-    string(REGEX REPLACE "hpp\\s" "cpp" CPP_FILE_NAME ${source})
-    string(REGEX REPLACE "h\\s" "cpp" CPP_FILE_NAME ${source})
-    message(${CPP_FILE_NAME})
-    set(CPP_FULL_NAME "${CMAKE_CURRENT_BINARY_DIR}/${CPP_FILE_NAME}")
-    set(H_FULL_NAME "${CMAKE_CURRENT_SOURCE_DIR}/${source}")
+    set(CPP_FULL_NAME "${CMAKE_CURRENT_BINARY_DIR}/${source}.cpp")
+    set(H_FULL_NAME "${CMAKE_CURRENT_SOURCE_DIR}/${source}.h")
     add_custom_command(
         OUTPUT "${CPP_FULL_NAME}"
         COMMAND ${CXXTESTGEN} --error-printer --output "${CPP_FULL_NAME}" "${H_FULL_NAME}" 
         DEPENDS "${H_FULL_NAME}"
-    )
+        )
     add_executable(${target} ${CPP_FULL_NAME})
     set_target_properties(${target} PROPERTIES COMPILE_FLAGS "-Wno-effc++")
-    add_test(${target} ${RUNTIME_OUTPUT_DIRECTORY}/${target})
+    add_test("${target}" ${RUNTIME_OUTPUT_DIRECTORY}/${target})
 endfunction(cxx_test)
